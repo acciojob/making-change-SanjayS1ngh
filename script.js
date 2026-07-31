79cf1823ba46565a51afcf36b099a25d1a7da9f6
Sanjay Singh <114378504+SanjayS1ngh@users.noreply.github.com>
2026-07-31 18:17:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,5 +22,5 @@ const makeChange = (c) => {
 	return ans;
 } 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
