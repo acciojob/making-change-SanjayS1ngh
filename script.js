7fe2c6ba97e0f275ed880d25c34a0a8d48451bd7
Sanjay Singh <114378504+SanjayS1ngh@users.noreply.github.com>
2026-07-31 16:09:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const makeChange = (c) => {
   // your name here
-};
-
+	let obj={
+		"q":25,
+		"d":10,
+		"n":5,
+		"p":1
+	};
+	let num=Number(c);
+	let ans={
+		"q":0,
+		"d":0,
+		"n":0,
+		"p":0
+	};
+	for(let i in obj){
+		while(c>obj[i]){
+			ans[i]++;
+			c-=obj[i];
+		}
+	}
+}
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
